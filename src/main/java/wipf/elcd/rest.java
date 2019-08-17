a37e69b3fc4052604c78f40f28aff2db8fa0c921
wipf <tobf@web.de>
2019-08-17 11:46:09 +0200

    vorUmbau

--- src/main/java/wipf/elcd/rest.java
+++ src/main/java/wipf/elcd/rest.java
@@ -1,19 +1,30 @@
 package wipf.elcd;
 
+import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-@Path("/elcd")
+@RequestScoped
+@Path("/wipf")
+//@Consumes({ "application/json" })
+//@Produces({ "application/json" })
 public class rest {
 
 	@GET
 	@Path("/go")
 	public Response responseMsg() {
 
-		String response = "Hello from: ";
+		String response = "Hello from: go";
 
 		return Response.status(200).entity(response).build();
+	}
+
+	@GET
+	@Path("/wipf")
+	public String responseMsg2() {
+
+		return "Hello from: wipf";
 	}
 
 }
