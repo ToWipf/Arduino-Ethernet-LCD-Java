66f6606369607a7cd9c761adf98b5d1d7476707b
wipf <wipf@wipf-Aspire-VN7-572G>
2019-10-09 21:30:08 +0200

    blowfish

--- src/main/java/org/wipf/elcd/model/MBlowfish.java
+++ src/main/java/org/wipf/elcd/model/MBlowfish.java
@@ -7,21 +7,29 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class MBlowfish {
 
-	public static String encrypt(String sIn) throws Exception {
-		byte[] keyData = ("superKey42").getBytes();
-		SecretKeySpec secretKeySpec = new SecretKeySpec(keyData, "Blowfish");
-		Cipher cipher = Cipher.getInstance("Blowfish");
-		cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-		byte[] hasil = cipher.doFinal(sIn.getBytes());
-		return Base64.getEncoder().encodeToString(hasil);
+	public static String sKey = "superKey42";
+
+	public static String encrypt(String sIn) {
+		try {
+			SecretKeySpec secretKeySpec = new SecretKeySpec((sKey).getBytes(), "Blowfish");
+			Cipher cipher = Cipher.getInstance("Blowfish");
+			cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+			byte[] hasil = cipher.doFinal(sIn.getBytes());
+			return Base64.getEncoder().encodeToString(hasil);
+		} catch (Exception e) {
+			return "fail";
+		}
 	}
 
-	public static String decrypt(String string) throws Exception {
-		byte[] keyData = ("superKey42").getBytes();
-		SecretKeySpec secretKeySpec = new SecretKeySpec(keyData, "Blowfish");
-		Cipher cipher = Cipher.getInstance("Blowfish");
-		cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-		byte[] hasil = cipher.doFinal(Base64.getDecoder().decode(string));
-		return new String(hasil);
+	public static String decrypt(String string) {
+		try {
+			SecretKeySpec secretKeySpec = new SecretKeySpec((sKey).getBytes(), "Blowfish");
+			Cipher cipher = Cipher.getInstance("Blowfish");
+			cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+			byte[] hasil = cipher.doFinal(Base64.getDecoder().decode(string));
+			return new String(hasil);
+		} catch (Exception e) {
+			return "fail";
+		}
 	}
 }
