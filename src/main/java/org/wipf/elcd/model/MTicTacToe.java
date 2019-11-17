b7e9411ebfbc21aa04d8c80c3bf2f7f65d26345a
Wipf <tobf@web.de>
2019-11-17 10:47:29 +0100

    ttt set

--- src/main/java/org/wipf/elcd/model/MTicTacToe.java
+++ src/main/java/org/wipf/elcd/model/MTicTacToe.java
@@ -120,6 +120,14 @@ public class MTicTacToe {
 				saveTicTacToe(ttt); // save game
 				return ttt.tttToNiceString();
 			}
+		case "setonly":
+			// setze feld ohne cpu
+			ttt.setByTelegram(t);
+			if (!ttt.setByNummer(t.getMessageInt(2), 'X')) {
+				return "Feld konnte nicht gesetzt werden";
+			} else {
+				saveTicTacToe(ttt); // save game
+			}
 		default:
 			return "Anleitung:\n\nttt neu: Neues Spiel\nttt setze NR: Setzen\nttt show: Zeige Feld\nttt raw: Zeige Feld";
 		}
