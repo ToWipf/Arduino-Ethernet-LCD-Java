c1749e01d3ee1f012893d38366721f9962fd4a57
Wipf <tobf@web.de>
2019-12-23 17:57:10 +0100

    menue

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,23 +5,24 @@ import { AppComponent } from './app.component';
 import { WipfComponent } from './wipf/wipf.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './components/footer/footer.component';
+import { TelegramComponent } from './components/telegram/telegram.component';
 import { FormsModule } from '@angular/forms';
 import { SidebarModule } from 'ng-sidebar';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TelegramComponent,
     FooterComponent,
-    WipfComponent,
-
+    WipfComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    SidebarModule.forRoot(),
+    SidebarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
