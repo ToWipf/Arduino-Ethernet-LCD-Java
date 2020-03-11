c2396fa281f5ecca13cdae88573d743b9391c8f6
wipf <tobf@web.de>
2020-03-11 19:15:40 +0100

    up

--- angular/src/app/components/telegram/telegram.component.ts
+++ angular/src/app/components/telegram/telegram.component.ts
@@ -1,16 +1,13 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-telegram',
-  templateUrl: './telegram.component.html',
-  styleUrls: ['./telegram.component.less']
+  selector: "app-telegram",
+  templateUrl: "./telegram.component.html",
+  styleUrls: ["./telegram.component.less"]
 })
 export class TelegramComponent {
-
-  constructor(
-    private http: HttpClient,
-  ) { }
+  constructor(private http: HttpClient) {}
 
   public sText: string;
   public sStatus: string;
@@ -18,10 +15,15 @@ export class TelegramComponent {
   public sendMsgToGroup(): void {
     if (this.sText) {
       //TODO: escape String
-      this.http.post('http://192.168.2.10:8080/telegram/sendMsgToGroup/' + this.sText, null).subscribe((resdata) => {
-        this.sStatus = resdata.toString();
-        this.sText = null;
-      });
+      this.http
+        .post(
+          "http://192.168.2.10:8080/telegram/sendMsgToGroup/" + this.sText,
+          null
+        )
+        .subscribe(resdata => {
+          this.sStatus = resdata.toString();
+          this.sText = null;
+        });
     }
   }
 }
--- angular/src/app/components/todolist/todol.component.ts
+++ angular/src/app/components/todolist/todol.component.ts
@@ -1,28 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-todoli',
-  templateUrl: './todol.component.html',
-  styleUrls: ['./todol.component.less']
+  selector: "app-todoli",
+  templateUrl: "./todol.component.html",
+  styleUrls: ["./todol.component.less"]
 })
 export class ToDoLiComponent implements OnInit {
-
-  constructor(
-    private http: HttpClient,
-  ) { }
+  constructor(private http: HttpClient) {}
+  public toarry: [{ id: number, data: string, active: string, editby: string }];
 
   ngOnInit() {
     this.getAll();
   }
-  
+
   private getAll(): void {
-    this.http.get("http://192.168.2.10:8080/todolist/getAllJson")
-      .subscribe((resdata) => {
-        const list = JSON.parse(resdata.toString());
-        console.log(list);
-        //TODO:
-        list.forEach((item) => {
+    this.http
+      .get("http://192.168.2.10:8080/todolist/getAllJson")
+      .subscribe((resdata: any) => {
+        this.toarry = resdata;
+
+        this.toarry.forEach(item => {
           console.log(item);
         });
       });
