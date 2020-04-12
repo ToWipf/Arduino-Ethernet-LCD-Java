8cea68de456c8b87128af0106acee8321fdacabe
wipf <tobf@web.de>
2020-04-13 01:23:48 +0200

    xmx

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar /root/wipfapp/wipfapp-1.0-SNAPSHOT-runner.jar >> /root/wipfapp/wipf.log 2>> /root/wipfapp/wipf.log &
+java -Xmx48m -jar /root/wipfapp/wipfapp-1.0-SNAPSHOT-runner.jar >> /root/wipfapp/wipf.log 2>> /root/wipfapp/wipf.log &
 #tail -f /root/wipfapp/log -n 0
